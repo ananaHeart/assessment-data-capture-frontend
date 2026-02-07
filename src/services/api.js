41b1e761c4c8d544208e4b5986878afd4cefc028
Heart Anana <2023_cete_ananahea@online.htcgsc.edu.ph>
2026-02-07 08:19:04 +0800

    Update API URL to Google Cloud

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // This creates a reusable instance of axios with our backend's base URL.
 // This way, we don't have to type 'http://localhost:3001/api' in every API call.
 const api = axios.create({
-  baseURL: 'https://assessment-data-capture-backend.onrender.com/api', 
+  baseURL: 'https://assessment-backend-api-202843053880.asia-southeast1.run.app', 
 });
 
 export default api;
